5e1ee6c534f5a01c62203213225cffc83e715cf0
BOMEN <bomen21x@gmail.com>
2022-06-18 22:11:12 +0700

    Update __manifest__.py

--- ctp_backend_font_style/__manifest__.py
+++ ctp_backend_font_style/__manifest__.py
@@ -2,6 +2,10 @@
 ###################################################################################
 #
 #    Cybernetics Plus Co., Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybernetics Plus Technologies (<https://www.cybernetics.plus>).
+#    Author: Cybernetics Plus Techno Solutions (<https://www.cybernetics.plus>)
+#
 #    Customize Web Backend Font Style
 #
 ###################################################################################
