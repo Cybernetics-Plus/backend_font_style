6fedbee73f61e4e97ef5486ff5a91e4e682ec598
BOMEN <bomen21x@gmail.com>
2022-11-21 18:18:39 +0700

    Update __manifest__.py

--- ctp_backend_font_style/__manifest__.py
+++ ctp_backend_font_style/__manifest__.py
@@ -12,36 +12,38 @@
 
 {
     "name": "Backend Font Style",
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Customize Web Backend Font Style
             .""",
-    'description': """ 
+    "description": """ 
             Customize Web Backend Font Style
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'price': 15.00,
-    'currency': 'USD',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "AGPL-3",
+    "price": 15.00,
+    "currency": "USD",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'depends': ['base'],
-    'data': [
-        'data/res_config.xml',
-        'views/res_config.xml',
+    "depends": ["base"],
+    "data": [
+        "data/res_config.xml",
+        "views/res_config.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'ctp_backend_font_style/static/src/js/backend_font_style.js',
-            'ctp_backend_font_style/static/src/css/backend_font_style.css',
+    "assets": {
+        "web.assets_backend": [
+            "ctp_backend_font_style/static/src/js/backend_font_style.js",
+            "ctp_backend_font_style/static/src/css/backend_font_style.css",
         ],
     },
 }
